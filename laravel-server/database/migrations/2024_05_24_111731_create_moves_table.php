5a6c84f4877289baf3a83f156a06b715ed9a76c2
Artur Allmann <Artur.Allmann@tptlive.ee>
2024-05-24 14:33:00 +0300

    create moves model

---
+++ laravel-server/database/migrations/2024_05_24_111731_create_moves_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use App\Models\Games;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('moves', function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(Games::class, );
+            $table->string('player', 50);
+            $table->integer('location');
+            $table->integer('order');
+            $table->timestamps('time_stamp');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('moves');
+    }
+};
