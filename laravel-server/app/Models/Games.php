09405cca24954e41e1b1f1e821dd9506738f35cc
Artur Allmann <Artur.Allmann@tptlive.ee>
2024-05-24 14:13:16 +0300

    create migration for games

---
+++ laravel-server/app/Models/Games.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Games extends Model
+{
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+
+    protected $fillable = [
+        'id',
+        'player_x',
+        'player_o',
+        'in_progress',
+        'winner',
+    ];
+}
---
+++ laravel-server/database/migrations/2024_05_24_105608_create_games_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('games', function (Blueprint $table) {
+            $table->id();
+            $table->string('player_x', 50);
+            $table->string('player_o', 50);
+            $table->boolean('in_progress')->default(true);
+            $table->string('winner', 50)->nullable();
+            $table->timestamps('created_at');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('games');
+    }
+};
