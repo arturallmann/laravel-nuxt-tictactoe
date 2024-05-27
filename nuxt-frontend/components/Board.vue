0955ed145936647d43feea9faf3eade4cc2fac04
Artur Allmann <Artur.Allmann@tptlive.ee>
2024-05-27 16:14:15 +0300

    added randomised starting player
    can only play by yourself

--- nuxt-frontend/components/Board.vue
+++ nuxt-frontend/components/Board.vue
@@ -12,22 +12,28 @@
 
 <script>
 import Square from './Square.vue';
+import TurnIndicator from './TurnIndicator.vue';
+//randomly chooses which symbol starts the game.
+const startingSide = Math.round(Math.random(0, 1)) === 1 ? 'X' : 'O';
 
 export default {
   name: 'Board',
   components: {
     Square,
+    TurnIndicator,
   },
   data() {
     return {
       squares: Array(9).fill(''),
+      currentPlayer: startingSide,
     };
   },
   methods: {
     handleSquareClick(index) {
-      console.log(`Square click handled: ${index}`); // Debugging log
+      console.log(`Square click handled: ${index}`);
       if (this.squares[index] === '') {
-        this.squares[index] = 'X';
+        this.squares[index] = this.currentPlayer;
+        this.currentPlayer = this.currentPlayer === 'X' ? 'O' : 'X';
       }
     },
   },
@@ -40,5 +46,7 @@ export default {
   grid-template-columns: repeat(3, 100px);
   grid-template-rows: repeat(3, 100px);
   gap: 5px;
+  background-color: darkgrey;
+  justify-content: center;
 }
 </style>
