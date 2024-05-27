3e2fa6f5c467203842b79ac6993218d508a2410c
Artur Allmann <Artur.Allmann@tptlive.ee>
2024-05-27 16:14:36 +0300

    Revert "added randomised starting player"
    This reverts commit 0955ed145936647d43feea9faf3eade4cc2fac04.

--- nuxt-frontend/components/Board.vue
+++ nuxt-frontend/components/Board.vue
@@ -12,28 +12,22 @@
 
 <script>
 import Square from './Square.vue';
-import TurnIndicator from './TurnIndicator.vue';
-//randomly chooses which symbol starts the game.
-const startingSide = Math.round(Math.random(0, 1)) === 1 ? 'X' : 'O';
 
 export default {
   name: 'Board',
   components: {
     Square,
-    TurnIndicator,
   },
   data() {
     return {
       squares: Array(9).fill(''),
-      currentPlayer: startingSide,
     };
   },
   methods: {
     handleSquareClick(index) {
-      console.log(`Square click handled: ${index}`);
+      console.log(`Square click handled: ${index}`); // Debugging log
       if (this.squares[index] === '') {
-        this.squares[index] = this.currentPlayer;
-        this.currentPlayer = this.currentPlayer === 'X' ? 'O' : 'X';
+        this.squares[index] = 'X';
       }
     },
   },
@@ -46,7 +40,5 @@ export default {
   grid-template-columns: repeat(3, 100px);
   grid-template-rows: repeat(3, 100px);
   gap: 5px;
-  background-color: darkgrey;
-  justify-content: center;
 }
 </style>
